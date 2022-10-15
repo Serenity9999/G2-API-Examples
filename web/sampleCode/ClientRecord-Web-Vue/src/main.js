bf3b8e585cdac01a19c83bc831b35a323ff083f5
Everest <zhaoqingbiao@corp.netease.com>
2022-10-15 10:22:28 +0800

    同步 g2 web demo 示例源码

--- web/sampleCode/ClientRecord-Web-Vue/src/main.js
+++ web/sampleCode/ClientRecord-Web-Vue/src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/styles/common.less'
+import * as echarts from 'echarts'
+//全局引入echarts
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
